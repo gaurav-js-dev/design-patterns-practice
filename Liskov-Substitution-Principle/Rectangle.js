59c00dbe5759e81faf114058a5d9799d079e9c31
Gaurav <gsharma.jai@salucro.com>
2022-07-06 00:08:37 +0530

    Updated LSP demo code

--- Liskov-Substitution-Principle/Rectangle.js
+++ Liskov-Substitution-Principle/Rectangle.js
@@ -1,17 +1,56 @@
 class Rectangle {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+
+  set width(value) {
+    this._width = value;
+  }
+  set height(value) {
+    this._height = value;
   }
 
   get area() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 
   toString() {
-    return `${this.width}×${this.height}`;
+    return `${this._width}×${this._height}`;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+  set width(value) {
+    this._width = this._height = value;
+  }
+
+  set height(value) {
+    this._width = this._height = value;
   }
 }
 
 let rc = new Rectangle(2, 3);
 console.log(rc.toString());
+// output 2 x 3
+
+let sq = new Square(5);
+console.log(sq.toString());
+
+// output undefined x undefined
+
+let useIt = function (rc) {
+  let width = rc._width;
+  rc.height = 10;
+  console.log(`Expected area of ${10 * width}, ` + `got ${rc.area}`);
+};
