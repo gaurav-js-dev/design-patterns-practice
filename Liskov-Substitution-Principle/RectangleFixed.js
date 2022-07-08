bda3ea726ffa3ba9bab1401726c651930eb52e74
Gaurav <gsharma.jai@salucro.com>
2022-07-08 18:31:54 +0530

    Updated rectangle fixed

---
+++ Liskov-Substitution-Principle/RectangleFixed.js
@@ -0,0 +1,32 @@
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+  get height() {
+    return this._height;
+  }
+
+  set width(value) {
+    this._width = value;
+  }
+  set height(value) {
+    this._height = value;
+  }
+
+  get area() {
+    return this._width * this._height;
+  }
+
+  get isSquare() {
+    return this._width === this._height;
+  }
+
+  toString() {
+    return `${this._width}×${this._height}`;
+  }
+}
