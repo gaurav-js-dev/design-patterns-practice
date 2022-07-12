86bf0a8049baf691580416c1765ffca89bac141d
Gaurav <gsharma.jai@salucro.com>
2022-07-12 21:20:13 +0530

    Updated fix code

--- Strategy/TextProcessor.js
+++ Strategy/TextProcessor.js
@@ -3,6 +3,33 @@ let OutputFormat = Object.freeze({
     html: 1
 });
 
+class ListStrategy {
+    start(buffer) { }
+    end(buffer) { }
+    addListItem(buffer, item) { }
+}
+
+class MarkdownListStrategy extends ListStrategy {
+    addListItem(buffer, item) {
+        buffer.push(` * ${item}`);
+    }
+}
+
+class HtmlListStrategy extends ListStrategy {
+    start(buffer) {
+        buffer.push('<ul>');
+    }
+
+    end(buffer) {
+        buffer.push('</ul>');
+    }
+
+    addListItem(buffer, item) {
+        buffer.push(`  <li>${item}</li>`)
+    }
+}
+
+
 setOutputFormat(format)
 {
     switch (format) {
