df72f5eaae809fc113a3968bb4830ff7b42c987e
Gaurav <gaurav.haxor@gmail.com>
2022-08-12 20:14:32 +0530

    Updated code

--- Composite/GraphicObject.js
+++ Composite/GraphicObject.js
@@ -8,5 +8,34 @@ class GraphicObject {
     this.color = undefined;
     this._name = name;
   }
+
+  print(buffer, depth) {
+    buffer.push("*".repeat(depth));
+    if (depth > 0) buffer.push(" ");
+    if (this.color) buffer.push(this.color + " ");
+    buffer.push(this.name);
+    buffer.push("\n");
+
+    for (let child of this.children) child.print(buffer, depth + 1);
+  }
+  toString() {
+    let buffer = [];
+    this.print(buffer, 0);
+    return buffer.join("");
+  }
 }
 GraphicObject.count = 0;
+
+class Circle extends GraphicObject {
+  constructor(color) {
+    super("Circle");
+    this.color = color;
+  }
+}
+
+class Square extends GraphicObject {
+  constructor(color) {
+    super("Square");
+    this.color = color;
+  }
+}
