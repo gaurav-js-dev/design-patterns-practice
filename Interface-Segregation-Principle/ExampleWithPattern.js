1ba8c53b579b656f72cd06fe19c8e6e11383a958
Gaurav <gaurav.haxor@gmail.com>
2022-08-15 00:29:38 +0530

    Finished isp

--- Interface-Segregation-Principle/ExampleWithPattern.js
+++ Interface-Segregation-Principle/ExampleWithPattern.js
@@ -7,3 +7,35 @@ class Employee {
     console.log("I arrived at work at " + new Date().toTimeString());
   };
 }
+const dishwasherFunctions = {
+  washTheDishes: () => {
+    console.log(`I am washing the dishes`);
+  },
+};
+
+const cookFunctions = {
+  cookTheFood: (meal) => {
+    console.log(`I am cooking the ${meal}`);
+  },
+};
+
+const cleanerFunctions = {
+  cleanTheFloor: () => {
+    console.log(`I am cleaning the Floor`);
+  },
+  cleanTheTable: () => {
+    console.log(`I am cleaning the Table`);
+  },
+};
+
+class Dishwasher extends Employee {}
+
+Object.assign(Dishwasher.prototype, dishwasherFunctions);
+
+class Cook extends Employee {}
+
+Object.assign(Cook.prototype, cookFunctions);
+
+class Cleaner extends Employee {}
+
+Object.assign(Cleaner.prototype, cleanerFunctions);
