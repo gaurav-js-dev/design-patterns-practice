e0fb8a0af9daec28aead76536b5498414e7d23da
Gaurav <gaurav.haxor@gmail.com>
2022-08-16 21:12:18 +0530

    WIP Saving progress lsp

--- Liskov-Substitution-Principle/ExampleWithPattern.js
+++ Liskov-Substitution-Principle/ExampleWithPattern.js
@@ -20,3 +20,43 @@ class Developer {
     console.log(" I create Restful APIs");
   }
 }
+
+class ReactDeveloper extends Developer {}
+class NodeJSDeveloper extends Developer {}
+
+class JenkinsDevOpsEngineer extends Developer {}
+
+const frontEndDeveloper = ({ name, company }) => ({
+  createHTML: () => {
+    console.log(`${name} from ${company} can create Wonderful User Interfaces`);
+  },
+});
+
+const backEndDeveloper = ({ name, company }) => ({
+  createDatabaseTable: () => {
+    console.log(
+      `${name} from ${company} can create Normalized Database Tables`
+    );
+  },
+  createEndpoints: () => {
+    console.log(`${name} from ${company} can create useful Restful APIs`);
+  },
+});
+
+const devOpsEngineer = ({ name, company }) => ({
+  createCICDPipeline: () => {
+    console.log(`${name} from ${company} can automate Deployments`);
+  },
+});
+
+const fullStackDeveloper = ({ name, company }) => {
+  // composition of all FE, BE, DEVOPS
+  const developer = { name, company };
+
+  return {
+    ...developer,
+    ...frontEndDeveloper(developer),
+    ...backEndDeveloper(developer),
+    ...devOpsEngineer(developer),
+  };
+};
