ebf43c536e825a99b94134adfa0a431b01fd1a40
Gaurav <gaurav.haxor@gmail.com>
2022-08-18 22:04:40 +0530

    Added decorator example

---
+++ Decorator/ExampleWithPattern.js
@@ -0,0 +1,25 @@
+class BasicArmor {
+  constructor(armorType, height = 40) {
+    this.armorType = armorType;
+    this.height = height;
+  }
+}
+
+// new instance of BasicArmor
+const chainArmor = new BasicArmor("chainArmor");
+
+// Decorators
+chainArmor.setColor = function (color) {
+  this.color = color;
+};
+
+chainArmor.setHeight = function (height) {
+  this.height = height;
+};
+
+// chainArmor Decorators calls
+chainArmor.setColor("grey");
+chainArmor.setHeight(33);
+
+console.log(chainArmor.height); // 33
+console.log(chainArmor.color); // "grey"
